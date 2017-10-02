1223690a82af84e31d20e9e46b4d641a5b4d1aa1
Jean Chassoul <chassoul@gmail.com>
2017-10-02 13:19:11 -0400

    thanks for all the fish!

--- examples/hello.erl
+++ examples/hello.erl
@@ -1,9 +1,9 @@
 #!/usr/bin/env escript
 %% -*- erlang -*-
 %%! -smp enable -sname hello_world -pa ../_rel/luerl_demo_release/lib/*/ebin
-
 main(_) ->
     io:format("..and now for something completely different!\n",[]),
+    % do some lua
     luerl:do("print(\"Hello, Robert!\")"),
     % execute a file
     luerl:dofile("./hello.lua"),
@@ -11,4 +11,5 @@ main(_) ->
     State0 = luerl:init(),
     {ok, Chunk, State1} = luerl:load("print(\"Hello, Chunk!\")", State0),
     {_Ret, _NewState} = luerl:do(Chunk, State1),
+    % Goodbye and thanks for all the fish!
     done.
