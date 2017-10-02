04c6040320be218c86f652423748508048bed3cc
Jean Chassoul <chassoul@gmail.com>
2017-10-02 18:41:47 -0400

    clean, clean, clean

--- ships/demo.sh
+++ ships/demo.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-DEMO_LIBS="$HOME/work/structures/treehouse/deps/luerl/ebin $HOME/work/structures/treehouse/deps/uuid/ebin $HOME/work/structures/treehouse/deps/chumak/ebin $HOME/work/structures/treehouse/deps/esdl2/ebin ./ebin"
+DEMO_LIBS="../deps/*/ebin ../ebin"
 
-# observer
-exec erl -smp enable +stbt db -sname sim -setcookie ship-demo -pa $DEMO_LIBS
+exec erl -smp enable +stbt db -sname sim -setcookie ship-demo -run observer -pa $DEMO_LIBS
