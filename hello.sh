2da6b7196f324c819b9e6eb5f12c0152d6186042
Jean Chassoul <chassoul@gmail.com>
2017-10-03 00:22:14 -0400

    hello world

--- hello.sh
+++ hello.sh
@@ -2,4 +2,4 @@
 
 DEMO_LIBS="./deps/*/ebin ./ebin"
 
-exec erl -smp enable +stbt db -sname hello -setcookie world -pa $DEMO_LIBS
+exec erl -smp enable +stbt db -sname hello -setcookie world -pa $DEMO_LIBS -s hello run -s init stop -noshell
